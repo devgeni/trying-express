086d7870d3a2c6f563df4ee8d64a9ab774ea1c97
Evgeni B <eujeni@list.ru>
2018-12-09 19:59:40 +0300

    332

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
 
-const articles = [{ title: 'Example' }];
+const { Article } = require('./db');
 
 app.set('port', process.env.PORT || 80);
 
@@ -9,26 +9,32 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/articles', (req, res, next) => {
-	res.send(articles);
+	Article.all((err, articles) => {
+		if (err) return next(err);
+		res.send(articles);
+	});
 });
 
-app.post('/articles', (req, res, next) => {
+/*app.post('/articles', (req, res, next) => {
 	const article = { title: req.body.title };
 	articles.push(article);
 	res.send(article);
-});
+});*/
 
 app.get('/articles/:id', (req, res, next) => {
-	const id = req.params.id;
-	console.log('Fetching: ', id);
-	res.send(articles[id]);
+	const { id } = req.params;
+	Article.find(id, (err, article) => {
+		if (err) return next(err);
+		res.send(article);
+	});
 });
 
 app.delete('/articles/:id', (req, res, next) => {
-	const id = req.params.id;
-	console.log('Deleting: ', id);
-	delete articles[id];
-	res.send({ message: 'Deleted' });
+	const { id } = req.params;
+	Article.delete(id, (err) => {
+		if (err) return next(err);
+		res.send({ message: 'Deleted' });
+	});
 });
 
 app.listen(app.get('port'), () => {
